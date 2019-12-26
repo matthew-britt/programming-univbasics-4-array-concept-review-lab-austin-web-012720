620f9fd7408d4b460bcdbd1850fc8aabb34a30a0
Matthew Britt <mwbritt@hotmail.com>
2019-12-26 16:48:34 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,29 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
+  new_array = []
+  array.length.times do |index|
+    new_array << array[index]
+  end
+  new_array.index(value_to_find)
+  
 end
 
 def find_max_value(array)
   # Add your solution here
+  new_array = []
+  array.length.times do |index|
+    new_array << array[index]
+  end
+  new_array.sort!
+  return new_array.last 
 end
 
 def find_min_value(array)
   # Add your solution here
+  new_array = []
+  array.length.times do |index|
+    new_array << array[index]
+  end
+  new_array.sort!
+  return new_array[0]
 end
